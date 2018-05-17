8047e59dc29fc5d25ed44d91c4aafc10fc4a5bb0
AyaAhmed1 <ayaahmed9424@gmail.com>
2018-05-17 16:00:21 +0200

    show lectures in course show

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,18 @@
 class CoursesController < InheritedResources::Base
   before_action :authenticate_user!
+  
+  def show 
+    @course= Course.find(params[:id])
+    @lecures= Lecture.where(course_id: params[:id])
+  end   
+  
   private
 
     def course_params
       params.require(:course).permit(:title)
     end
+
+   
+   
 end
 
